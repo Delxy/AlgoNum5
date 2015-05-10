dd105163488e4b7a4432995bf99cde3a97c38ff8
Alkinn <t.henri.al@gmail.com>
2015-05-10 21:46:13 +0200

    Correction commentaires

--- src/part3.py
+++ src/part3.py
@@ -7,11 +7,11 @@ import part2
 
 
 def f_lambda(L, f, min_or_max):
-    """This function returns a lambda which describes the airflow arround f for a given lambda L. If f describe the extrados, the max value of f should be given (and the min value for the intrados"""
+    """This function returns a lambda-expression which describes the airflow arround f for a given lambda L. If f describe the extrados, the max value of f should be given (and the min value for the intrados"""
     return lambda x: (1-L)*f(x) + L*min_or_max*3
 
 def transfer(value, h_max, h_min, acc):
-    """This fonction returns a value which is modified to be well save in the matrix"""
+    """This fonction returns a value which is modified to be well saved in the matrix"""
     if(value > 0):
         return -int(value*acc-3*acc*h_min)
     else:
@@ -20,17 +20,17 @@ def transfer(value, h_max, h_min, acc):
 
 
 def pressure_map(filename,nb_slice,integ_methode):
-    """ This compute compute the pressure map around an airfoil, which is in the file filename with the given slice number. """
+    """ This computes the pressure map around an airfoil, which is in the file <<filename>> with the given slice number. """
     # e = extrados
     # i = intrados
-    #first, we need to parse the file which contain data.
+    #first, we need to parse the file which contains data.
     (ex, ey, ix, iy) = part1.load_foil(filename)
 
     # h_max and h_min are the max value and min value of the extrados and the intrados
     h_max = np.amax(ey)
     h_min = np.amin(iy)
 
-    # Here we build the result matrix, with an accuracy which allow us to store more different value, then the pressure map is closer to the reality.
+    # Here we build the result matrix, with an accuracy which allows us to store more different values, then the pressure map is closer to the reality.
     accuracy = nb_slice
     matrix_length = int(3* accuracy *(h_max-h_min))
     matrix = np.zeros([matrix_length, matrix_length])
@@ -39,10 +39,10 @@ def pressure_map(filename,nb_slice,integ_methode):
     f_upper = part1.interpolation_do(ex, ey)
     f_lower = part1.interpolation_do(ix, iy)
 
-    # Curves are interpolated, so we can chose all value for x in the interpolation range.
+    # Curves are interpolated, so we can choose every values for x in the interpolation range.
     x_values = np.arange(0, ex[ex.shape[0]-1], 1/matrix_length)
     
-    # The first loop is for all slice.
+    # The first loop is for every slices.
     for j in np.arange(0,1, (1.0/nb_slice)):
     
         # EXTRADOS
@@ -65,7 +65,7 @@ def pressure_map(filename,nb_slice,integ_methode):
             matrix[x, transfer(res, h_max, h_min, accuracy)] = 500*(size**2)
 
     
-    # Then the curve is displayed and the pressure map afterward.
+    # Then the curve is displayed and the pressure map afterwards.
     part1.display(ex, ey, ix, iy)
     plt.imshow(np.transpose(matrix), interpolation = 'nearest')
     plt.show()
